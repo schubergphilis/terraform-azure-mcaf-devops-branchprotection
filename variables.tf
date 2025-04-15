45fc1b5ef6407d111040d606249b14d1d79babca
Nelesh Goel <ngoel@schubergphilis.com>
2025-04-15 10:38:57 +0200

    on_push_reset_approved_votes

--- variables.tf
+++ variables.tf
@@ -44,7 +44,7 @@ EOT
     submitter_can_vote                     = optional(bool, false)
     last_pusher_cannot_approve             = optional(bool, false)
     allow_completion_with_rejects_or_waits = optional(bool, false)
-    on_push_reset_approved_votes           = optional(bool, false)
+    on_push_reset_approved_votes           = optional(bool, true)
     on_last_iteration_require_vote         = optional(bool, false)
     on_push_reset_all_votes                = optional(bool, false)
   })
