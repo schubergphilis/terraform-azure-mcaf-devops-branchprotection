9bb3629084891145f696ea573d1482d628ad5c02
Nelesh Goel <ngoel@schubergphilis.com>
2025-04-15 10:33:26 +0200

    default values

--- variables.tf
+++ variables.tf
@@ -42,9 +42,9 @@ EOT
   type = object({
     reviewer_count                         = number
     submitter_can_vote                     = optional(bool, false)
-    last_pusher_cannot_approve             = optional(bool, true)
+    last_pusher_cannot_approve             = optional(bool, false)
     allow_completion_with_rejects_or_waits = optional(bool, false)
-    on_push_reset_approved_votes           = optional(bool, true)
+    on_push_reset_approved_votes           = optional(bool, false)
     on_last_iteration_require_vote         = optional(bool, false)
     on_push_reset_all_votes                = optional(bool, false)
   })
