330df389a08029e04e0962b3e155ae8b1ad378a2
Nelesh Goel <ngoel@schubergphilis.com>
2025-04-22 11:32:57 +0200

    Added basic example

---
+++ examples/basic/main.tf
@@ -0,0 +1,74 @@
+terraform {
+  required_version = ">= 1.9"
+  backend "azurerm" {
+  }
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 4"
+    }
+
+    azuredevops = {
+      source  = "microsoft/azuredevops"
+      version = "1.6.0"
+    }
+  }
+}
+
+provider "azuredevops" {
+  org_service_url = "https://dev.azure.com/example-orgname"
+}
+
+module "azdo_branch_protection" {
+  source = "../.."
+  azure_devops = {
+    org          = "example-orgname"
+    project_name = "example-project"
+  }
+  name       = "example-workload"
+  short_name = "short-example-workload"
+  location   = "eastus"
+  azuredevops_branch_policy_auto_reviewers = {
+    enabled            = true
+    blocking           = true
+    submitter_can_vote = false
+    group_names        = ["Example Team"]
+    message            = "Code Reviewers have been automatically assigned to this pull request"
+
+  }
+
+  branch_policy_min_reviewers_settings = {
+    reviewer_count                         = 1
+    last_pusher_cannot_approve             = true
+    allow_completion_with_rejects_or_waits = true
+    on_last_iteration_require_vote         = true
+  }
+
+  azuredevops_branch_policy_comment_resolution = {
+    enabled  = true
+    blocking = true
+  }
+
+  azuredevops_branch_policy_merge_types = {
+    enabled                       = true
+    blocking                      = true
+    allow_squash                  = true
+    allow_rebase_and_fast_forward = true
+    allow_basic_no_fast_forward   = false
+    allow_rebase_with_merge       = false
+  }
+
+  azuredevops_branch_policy_build_validation = {
+    enabled                     = true
+    blocking                    = false
+    valid_duration              = 0
+    manual_queue_only           = false
+    queue_on_source_update_only = false
+    suffix                      = "branch-validate"
+  }
+}
+
+
+
+
